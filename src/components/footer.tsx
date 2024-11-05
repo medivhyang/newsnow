0f955407c13e607cdbc4b0c38f1cde7a2f9fe8f4
Medivh Yang <medivhwork@163.com>
2024-11-06 00:24:05 +0800

    修正Footer中LICENSE链接

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,9 @@ import { Author, Homepage } from "@shared/consts"
 export function Footer() {
   return (
     <>
-      <a href={`${Homepage}/LICENCE`} target="_blank">MIT LICENCE</a>
+      <a href={`${Homepage}/blob/main/LICENSE`} target="_blank">
+        MIT LICENCE
+      </a>
       <span>
         <span>NewsNow © 2024 By </span>
         <a href={Author.url} target="_blank">
